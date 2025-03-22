44af2cca0231e5989af271570448a819d685bc25
sureka-95 <surekavj@gmail.com>
2025-03-22 13:00:39 +0530

    string is num or not

---
+++ string is num.py
@@ -0,0 +1,12 @@
+# Import the date and time module
+import datetime
+
+# to get the curret date & time
+a=datetime.datetime.now()
+
+# in above the date and time of now is store in var a
+# to get output
+
+print (a)
+
+
